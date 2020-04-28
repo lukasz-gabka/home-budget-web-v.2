0c4964eefd5c39df02945ee3ea26b76d981d76d2
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2020-04-28 22:15:03 +0200

    added blank database

--- eSwinka.sql
+++ eSwinka.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Czas generowania: 23 Kwi 2020, 22:13
+-- Czas generowania: 28 Kwi 2020, 22:12
 -- Wersja serwera: 5.7.29-0ubuntu0.18.04.1
 -- Wersja PHP: 7.2.24-0ubuntu0.18.04.4
 
@@ -19,6 +19,52 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `eSwinka`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenseCategories`
+--
+
+CREATE TABLE `expenseCategories` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenseCategoriesDefault`
+--
+
+CREATE TABLE `expenseCategoriesDefault` (
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `expenseCategoriesDefault`
+--
+
+INSERT INTO `expenseCategoriesDefault` (`id`, `name`) VALUES
+(1, 'Jedzenie'),
+(2, 'Mieszkanie'),
+(3, 'Transport'),
+(4, 'Telekomunikacja'),
+(5, 'Opieka zdrowotna'),
+(6, 'Ubranie'),
+(7, 'Higiena'),
+(8, 'Dzieci'),
+(9, 'Rozrywka'),
+(10, 'Wycieczka'),
+(11, 'Szkolenia'),
+(12, 'Książki'),
+(13, 'Oszczędności'),
+(14, 'Na złotą jesień, czyli emeryturę'),
+(15, 'Spłata długów'),
+(16, 'Darowizna'),
+(17, 'Inne wydatki');
 
 -- --------------------------------------------------------
 
@@ -39,6 +85,39 @@ CREATE TABLE `expenses` (
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `incomeCategories`
+--
+
+CREATE TABLE `incomeCategories` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomeCategoriesDefault`
+--
+
+CREATE TABLE `incomeCategoriesDefault` (
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `incomeCategoriesDefault`
+--
+
+INSERT INTO `incomeCategoriesDefault` (`id`, `name`) VALUES
+(1, 'Wynagrodzenie'),
+(2, 'Odsetki bankowe'),
+(3, 'Sprzedaż na Allegro'),
+(4, 'Inne');
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `incomes`
 --
 
@@ -50,6 +129,39 @@ CREATE TABLE `incomes` (
   `comment` text CHARACTER SET utf8 COLLATE utf8_polish_ci,
   `userId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `paymentMethods`
+--
+
+CREATE TABLE `paymentMethods` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `paymentMethodsDefault`
+--
+
+CREATE TABLE `paymentMethodsDefault` (
+  `id` int(11) NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `paymentMethodsDefault`
+--
+
+INSERT INTO `paymentMethodsDefault` (`id`, `name`) VALUES
+(1, 'Gotówka'),
+(2, 'Przelew'),
+(3, 'Karta kredytowa'),
+(4, 'Karta debetowa');
 
 -- --------------------------------------------------------
 
@@ -69,15 +181,51 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `expenseCategories`
+--
+ALTER TABLE `expenseCategories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `expenseCategoriesDefault`
+--
+ALTER TABLE `expenseCategoriesDefault`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `expenses`
 --
 ALTER TABLE `expenses`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `incomeCategories`
+--
+ALTER TABLE `incomeCategories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `incomeCategoriesDefault`
+--
+ALTER TABLE `incomeCategoriesDefault`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `incomes`
 --
 ALTER TABLE `incomes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paymentMethods`
+--
+ALTER TABLE `paymentMethods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paymentMethodsDefault`
+--
+ALTER TABLE `paymentMethodsDefault`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -91,15 +239,45 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `expenseCategories`
+--
+ALTER TABLE `expenseCategories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `expenseCategoriesDefault`
+--
+ALTER TABLE `expenseCategoriesDefault`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
 -- AUTO_INCREMENT dla tabeli `expenses`
 --
 ALTER TABLE `expenses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT dla tabeli `incomeCategories`
+--
+ALTER TABLE `incomeCategories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `incomeCategoriesDefault`
+--
+ALTER TABLE `incomeCategoriesDefault`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT dla tabeli `incomes`
 --
 ALTER TABLE `incomes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `paymentMethods`
+--
+ALTER TABLE `paymentMethods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT dla tabeli `paymentMethodsDefault`
+--
+ALTER TABLE `paymentMethodsDefault`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
